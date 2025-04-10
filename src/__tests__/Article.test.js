187e9001c5f45fb4cc99d8c2d5b46fa5e42e0091
ian <ianwisdom90@gmail.com>
2025-04-11 01:11:56 +0300

    article

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -4,8 +4,12 @@ import React from "react";
 
 import Article from "../components/Article";
 
+// src/__tests__/Article.test.js
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
+
+  // add this line
+  screen.debug();
 
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
--- src/components/Article.js
+++ src/components/Article.js
@@ -3,6 +3,8 @@ import React from "react";
 function Article() {
   return (
     <div>
+      <p>please pass this test</p>
+
       {/* please pass this test */}
     </div>
   );
